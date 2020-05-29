5d5947cd351a07f04adb54501f230ee7ebf659d8
Namita Dongre <ndongre@andrew.cmu.edu>
2020-05-28 21:38:21 -0400

    More exception handling details implemented such as retries

--- parser.py
+++ parser.py
@@ -1,23 +1,47 @@
 import requests
-
-def get_webpage(url):
+ 
+def get_webpage(url, attempt):
+	### TODO: create a log
+	numAttempts = 2
+	html = None
+	status = "OK: Succesfully requested and recieved HTML from: " + url
+	status_code = 0
 	try:
-		html = requests.get(url)
+		### TODO: log event of reading url
+		html = requests.get(url, timeout=0.1)
 		html.raise_for_status()
-		print(html.text)
+		status_code = 1
 	except requests.exceptions.HTTPError as e:
-		print("System encountered an HTTP error. See logs for more details.")
-	except requests.exceptions.Timeout:
-		### TODO: attempt to reconnect 3 times, let user know
-	    print("System encountered a Timeout error and attempted to re-connect 3 times. See logs for more details.")
-	except requests.exceptions.TooManyRedirects:
-	    print("System encountered TooManyRedirects error. Please try a different URL. See logs for more details.")
+		status = "ERROR: System encountered an HTTP error. See logs for more details."
+	except requests.exceptions.Timeout as e:
+		if (attempt < numAttempts):
+			print("System encountered a Timeout error after waiting 50ms. Trying attempt", attempt + 1, "of 2.")
+			get_webpage(url, attempt+1)
+		else:
+		    status = (
+		    	"ERROR: System encountered Timeout errors after two attempts to re-connect."
+		    	" See logs for more details."
+		    )
+	except requests.exceptions.TooManyRedirects as e:
+	    status = (
+	    	"ERROR: System encountered TooManyRedirects error. Please try a different URL."
+	    	"See logs for more details."
+	    )
 	except requests.exceptions.ConnectionError as e:
-		print("System encountered a Connection error. There is most-likely a network problem. See logs for more details.")
+		status = (
+			"ERROR: System encountered a Connection error. There is most-likely a network problem."
+			"See logs for more details."
+		)
 	except requests.exceptions.RequestException as e:
-		print("System threw a Request Exception. Please see logs for more details.")
+		status = "ERROR: System threw a Request Exception. Please see logs for more details."
+
+	return html.text if status_code else None
+
+# def parse()
 
 
 if __name__ == "__main__":
-	url = "https://xdxd"
-	get_webpage(url)
+	## TODO: User input of URL
+	url = "https://helpx.adobe.com/security/products/magento/apsb20-02.html"
+	# url = "http://example.com:81"
+	get_webpage(url, 1)
